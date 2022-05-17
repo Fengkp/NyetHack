db8e734121503b8b9daf570b3f53ba7378d301db
Feng <parrafk@gmail.com>
2022-05-17 15:16:46 -0400

    initial commit of Textbook project

---
+++ src/Game.kt
@@ -0,0 +1,51 @@
+// Start of a game called NyetHack, based on NetHack
+fun main(args: Array<String>) {
+    val name = "Madrigal"
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+
+    // Aura
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    // Player status
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    // when (Switch statement)
+    val race = "gnome"
+    val faction = when (race) {
+        "dwarf" -> "Keepers of the Mines"
+        "gnome" -> "Keepers of the Mines"
+        "orc" -> "Free People of the Rolling Hills"
+        "human" -> "Free People of the Rolling Hills"
+        else -> "No race"
+    }
+}
+
+private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String) {
+    println("(Aura: $auraColor) " + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
+
+// visibility modifier; function declaration keyword; function name; function parameters; return type
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches"
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds, but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+    return healthStatus
+}
